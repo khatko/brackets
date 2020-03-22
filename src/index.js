81ba992730437abc01a0ca58ad13e5f76e3708bc
Natalie <natali14441@gmail.com>
2020-03-22 20:35:13 +0300

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let configs = bracketsConfig.map(c => c.join(""));
+
+    let startLength = str.length; 
+    let repeat = true;
+
+    while(repeat) {
+        configs.forEach(c => {
+            str = str.replace(c, "");
+        });
+
+        repeat = startLength != str.length;
+        startLength = str.length;
+    }
+
+    return str.length == 0;    
 }
